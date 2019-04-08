8999c6e9feacfdef8c8c8fbc890c63b7523e96fa
[lilphunky] <[phamlily99@gmail.com]>
2019-04-08 08:17:18 -0700

    Added tumblrlab starter files

---
+++ StarterProjects/TumblrLab/TumblrLab/Controllers/PhotosViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotosViewController.swift
+//  TumblrLab
+//
+//  Created by Lily Pham on 4/7/19.
+//  Copyright © 2019 Lily Pham. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    // Initialize to empty array so we don't worry about it being nil later
+    var posts: [[String: Any]] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
