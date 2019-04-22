6e8c42d6cb10fa4cb2d4267372de82e0ab6dd795
Hermain Hanif <hmhanif@uci.edu>
2019-04-22 08:28:44 -0700

    added maps

--- CompletedProjects/ios_photo_map-master/Photo Map/PhotoMapViewController.swift
+++ CompletedProjects/ios_photo_map-master/Photo Map/PhotoMapViewController.swift
@@ -11,12 +11,26 @@ import MapKit
 
 class PhotoMapViewController: UIViewController, LocationsViewControllerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        set_Inital_Location()
     }
 
+    /* Set initial location to San Francisco */
+    func set_Inital_Location(){
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
